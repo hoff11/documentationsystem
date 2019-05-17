3ba3251c80641aacbf93d5617efe6c28da0e86c9
Micah Hoffmann <micah.j.hoffmann@boeing.com>
2019-05-17 13:13:08 -0700

    building out api

---
+++ DocuDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DocuDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ DocuDataManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DocuDataManager.WebApiApplication" Language="C#" %>
