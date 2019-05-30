3f6b8925ca062b5d660d41723e777b91e9ad44ef
Micah Hoffmann <micah.j.hoffmann@boeing.com>
2019-05-30 07:27:45 -0700

    Data access class library

---
+++ DocumentData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id NVARCHAR(128)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT Id, FirstName, LastName, EmailAddress, CreatedDate
+	FROM [dbo].[USER]
+    WHERE Id = @Id
+end
