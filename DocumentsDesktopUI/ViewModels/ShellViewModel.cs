126deb3a75b6e8015771b1e091ba9dc4e397bf66
Micah Hoffmann <micah.j.hoffmann@boeing.com>
2019-06-10 12:55:28 -0700

    Replace containter refrence with IoC refrence instead in shellVM

--- DocumentsDesktopUI/ViewModels/ShellViewModel.cs
+++ DocumentsDesktopUI/ViewModels/ShellViewModel.cs
@@ -12,18 +12,14 @@ namespace DocumentsDesktopUI.ViewModels
     {
         private IEventAggregator _events;
         private SalesViewModel _salesVm;
-        private SimpleContainer _container;
-
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVm,
-            SimpleContainer conatiner)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVm)
         {
             _events = events;
             _salesVm = salesVm;
-            _container = conatiner;
 
             _events.Subscribe(this);
 
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
