99f50b49b2455bc503e520476bb44e6a380ae345
Micah Hoffmann <micah.j.hoffmann@boeing.com>
2019-06-10 12:58:02 -0700

    Simply tax claulation, instead use Linq statement

--- DocumentsDesktopUI/ViewModels/SalesViewModel.cs
+++ DocumentsDesktopUI/ViewModels/SalesViewModel.cs
@@ -92,13 +92,17 @@ namespace DocumentsDesktopUI.ViewModels
         {
             decimal taxAmount = 0;
             decimal taxRate = _configHelper.GetTaxRate()/100;
-            foreach (var item in Cart)
-            {
-                if (item.Product.IsTaxable)
-                {
-                    taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate);
-                }
-            }
+            //foreach (var item in Cart)
+            //{
+            //    if (item.Product.IsTaxable)
+            //    {
+            //        taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate);
+            //    }
+            //}
+            taxAmount = Cart
+                .Where(x => x.Product.IsTaxable)
+                .Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate);
+                
             return taxAmount;
         }
         public string Tax
